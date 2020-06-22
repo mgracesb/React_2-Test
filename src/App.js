1b05657966f857132a66fce3870d2007f94775aa
mgracesb <mercysbarcel@gmail.com>
2020-06-22 19:37:02 +0200

    add faker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import faker from "faker";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="ui container comments">
+          <div className="comment">
+            <a className="avatar" href="/">
+              <img alt="avatar" src={faker.image.avatar} />
+            </a>
+            <div className="content">
+              <a className="author" href="/">
+                Sam
+              </a>
+              <div className="metadata">
+                <span className="date">Today at 6:00PM</span>
+              </div>
+            </div>
+            <div className="text">Nice blog post!</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

7d2f52e8041c65d9bc8d1410a3cdcc758f8441f3
mgracesb <mercysbarcel@gmail.com>
2020-07-02 18:48:55 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -6,46 +6,55 @@ import SeasonDisplay from "./components/SeasonDisplay.js";
 import "./styles/app.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMsg: "" };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMsg: err.message })
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return (
-      <div className="App">
-        <SeasonDisplay />
-        <div className="ui container comments">
-          <ApprovalCard>
-            <CommentDetail
-              name="Sammyn"
-              timeAgo="Today 16:05"
-              comment="I like the subject"
-              avatar={faker.image.avatar()}
-            />
-          </ApprovalCard>
-          <ApprovalCard>
-            <CommentDetail
-              name="Christine"
-              timeAgo="Today 17:53"
-              comment="The content is amazing"
-              avatar={faker.image.avatar()}
-            />
-          </ApprovalCard>
-          <ApprovalCard>
-            <CommentDetail
-              name="Elaine"
-              timeAgo="Yesterday 21:15"
-              comment="Nice blog post"
-              avatar={faker.image.avatar()}
-            />
-          </ApprovalCard>
-        </div>
-      </div>
-    );
+    if (this.state.errorMsg && !this.state.lat) {
+      return <p>Error: {this.state.errorMsg}</p>;
+    }
+    if (!this.state.errorMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
+    // return (
+    //   <div className="App">
+    //     {/* <SeasonDisplay lat={lat} /> */}
+
+    //     <div className="ui container comments">
+    //       <ApprovalCard>
+    //         <CommentDetail
+    //           name="Sammyn"
+    //           timeAgo="Today 16:05"
+    //           comment="I like the subject"
+    //           avatar={faker.image.avatar()}
+    //         />
+    //       </ApprovalCard>
+    //       <ApprovalCard>
+    //         <CommentDetail
+    //           name="Christine"
+    //           timeAgo="Today 17:53"
+    //           comment="The content is amazing"
+    //           avatar={faker.image.avatar()}
+    //         />
+    //       </ApprovalCard>
+    //       <ApprovalCard>
+    //         <CommentDetail
+    //           name="Elaine"
+    //           timeAgo="Yesterday 21:15"
+    //           comment="Nice blog post"
+    //           avatar={faker.image.avatar()}
+    //         />
+    //       </ApprovalCard>
+    //     </div>
+    //   </div>
+    // );
   }
 }
 
